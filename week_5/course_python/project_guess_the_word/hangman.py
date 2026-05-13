41880e77a567e17c7dbc932bcf2ed7af4a3ae7e8
Ishay Gavrieli <hagit3172@gmail.com>
2026-05-13 17:13:20 +0300

    update the code

--- week_5/course_python/project_guess_the_word/hangman.py
+++ week_5/course_python/project_guess_the_word/hangman.py
@@ -35,6 +35,8 @@ def get_valid_guess(already_guess: list):
                 print("You already guessed that letter.")
                 continue
             return user_guess
+        print("you can only type letters please try again")
+        continue
 
 def update_hidden_word(user_guess, secret_word, hidden_word):
     found = False
