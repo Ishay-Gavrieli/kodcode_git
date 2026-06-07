fc57dd8da4964281f4f01dc091509137752cba58
Ishay Gavrieli <hagit3172@gmail.com>
2026-06-07 21:58:56 +0300

    add setup

---
+++ week_9/mysql/setup.py
@@ -0,0 +1,31 @@
+import mysql.connector
+import time
+
+time.sleep(2)
+
+conn = mysql.connector.connect(
+    host="localhost",
+    port=3306,
+    user="root",
+    password="root",
+    database="soldiers_db"
+)
+
+create_table_sql = """
+CREATE TABLE IF NOT EXISTS soldiers (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    `rank` VARCHAR(50),
+    unit VARCHAR(100),
+    active BOOLEAN DEFAULT TRUE
+);
+"""
+
+cursor = conn.cursor()
+# cursor.execute(create_table_sql) 
+cursor.execute('select * from soldiers')
+print(cursor.fetchall())
+conn.commit()
+print("Table created successfully.")
+cursor.close()
+conn.close()
