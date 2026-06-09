92d6477635ae80a38faa267ac2f34ce9d5ecc586
Ishay Gavrieli <hagit3172@gmail.com>
2026-06-09 22:43:02 +0300

    add changes

--- week_9/mysql/main.py
+++ week_9/mysql/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routers import router
+import uvicorn
 
 app = FastAPI()
 
@@ -8,3 +9,7 @@ app.include_router(router)
 @app.get("/")
 def read_root():
     return {"message": "Soldiers API is running"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app",host="127.0.0.1",port=8000,reload=True)
